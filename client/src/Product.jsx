f4a18989d951dc9d341cac7b62a70640a9aeee68
Manesha-99 <maneshatharindaofficial@gmail.com>
2024-08-30 22:43:05 +0530

    Finalized project. need to apply some styles

--- client/src/Product.jsx
+++ client/src/Product.jsx
@@ -1,9 +1,86 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Product = () => {
-  return (
-    <div className='bg-slate-500'><h1>Shopping Cart</h1></div>
-  )
-}
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [searchItem, setSearchItem] = useState("");
 
-export default Product
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const [productsResponse, categoryResponse] = await Promise.all([
+          axios.get("http://localhost:3001/api/products"),
+          axios.get("http://localhost:3001/api/categories"),
+        ]);
+        setProducts(productsResponse.data);
+        setCategories(categoryResponse.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getData();
+  }, []);
+
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+  const handleSearchChange = (event) => {
+    setSearchItem(event.target.value);
+  };
+
+  const filteredProducts = products.filter(
+    (product) =>
+      (selectedCategory === "" || product.category === selectedCategory) &&
+      product.name.toLowerCase().includes(searchItem.toLowerCase())
+  );
+
+  return (
+    <div className="flex flex-col justify-center bg-slate-300">
+      <div className="bg-slate-600 p-4 rounded max-w-[600px] w-full mx-auto m-4">
+        <h1 className="text-white text-center text-[30px] p-4">
+          Shopping Cart
+        </h1>
+      </div>
+      <div className="bg-slate-600 p-4 rounded max-w-[600px] w-full mx-auto m-4 ">
+        <input
+          type="text"
+          placeholder="Search Products...."
+          className="rounded p-4 max-w-[250px] w-full text-center "
+          value={searchItem}
+          onChange={handleSearchChange}
+        />
+        <select
+          className="rounded max-w-[250px] w-full p-4 ml-[60px] text-center cursor-pointer"
+          value={selectedCategory}
+          onChange={handleCategoryChange}
+        >
+          <option className="bg-slate-300" value="">
+            All Categories
+          </option>
+          {categories.map((category, index) => (
+            <option className="bg-slate-300" key={index} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="">
+        {filteredProducts.map((product) => (
+          <div
+            className="bg-slate-500 p-4 rounded max-w-[600px] w-full mx-auto m-4 text-center text-white"
+            key={product.id}
+          >
+            <h2 className="text-[30px]">{product.name}</h2>
+            <p className="text-[20px]">Category: {product.category}</p>
+            <p className="text-[15px]">Price: ${product.price}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Product;
--- server/index.js
+++ server/index.js
@@ -1,28 +1,30 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from "express"
+import dotenv from "dotenv"
+import cors from 'cors'
 
 const app = express();
 app.use(express.json());
 dotenv.config();
+app.use(cors());
 
 const port = process.env.PORT;
 
-//--------HardCoded Data-----------------
+//--------------------------HardCoded Data--------------------------
 
 const products = [
-  { id: 1, name: "Product ex_1", category: "Category_1" },
-  { id: 2, name: "Product ex_2", category: "Category_1" },
-  { id: 3, name: "Product ex_3", category: "Category_2" },
-  { id: 4, name: "Product ex_4", category: "Category_2" },
-  { id: 5, name: "Product ex_5", category: "Category_3" },
-  { id: 6, name: "Product ex_6", category: "Category_3" },
-  { id: 7, name: "Product ex_7", category: "Category_4" },
-  { id: 8, name: "Product ex_8", category: "Category_4" },
+  { id: 1, name: "Product ex_1", category: "Category_1", price: "9.99"},
+  { id: 2, name: "Product ex_2", category: "Category_1", price: "19.99" },
+  { id: 3, name: "Product ex_3", category: "Category_2", price: "6.99" },
+  { id: 4, name: "Product ex_4", category: "Category_2", price: "16.99" },
+  { id: 5, name: "Product ex_5", category: "Category_3", price: "8.99" },
+  { id: 6, name: "Product ex_6", category: "Category_3", price: "18.99" },
+  { id: 7, name: "Product ex_7", category: "Category_4", price: "12.99" },
+  { id: 8, name: "Product ex_8", category: "Category_4", price: "22.99" },
 ];
 
 const categories = ["Category_1", "Category_2", "Category_3", "Category_4"];
 
-//------------------------End Points-------------------------
+//--------------------------End Points------------------------------
 
 app.get("/api/products", (req, res) => {
   res.status(200).json(products);
